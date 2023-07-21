df6984423260c680d05ed4bf63eadd334b3f72d3
Mendes <lucas.mendessilva@hotmail.com>
2023-07-21 13:02:53 -0300

    add email validation

--- js/script.js
+++ js/script.js
@@ -1,20 +1,66 @@
-function checkScrollBar() {
+// import validator from 'validator';
 
-  const img = document.querySelector('.image-container img');
+function checkScrollBar() {
+  const img = document.querySelector(".image-container img");
 
   const hasScrollbar = document.body.scrollWidth > window.innerWidth;
 
-  const isTablet = (window.innerWidth <= 850);
+  const isTablet = window.innerWidth <= 850;
 
-  const isMobile = window.innerWidth < 570
+  const isMobile = window.innerWidth < 570;
 
-  if(isTablet || hasScrollbar || isMobile) {
-    img.src = '/assets/images/illustration-sign-up-mobile.svg'
+  if (isTablet || hasScrollbar || isMobile) {
+    img.src = "/assets/images/illustration-sign-up-mobile.svg";
   } else {
-    img.src = '/assets/images/illustration-sign-up-desktop.svg'
+    img.src = "/assets/images/illustration-sign-up-desktop.svg";
   }
 }
 
-window.addEventListener('load', checkScrollBar);
+const inputElement = document.querySelector('#email');
+const containerMessage = document.querySelector('.container-message');
 
-window.addEventListener('resize', checkScrollBar);
+function checkErrorMessage() {
+  const errorMessage = containerMessage.querySelector('.error-message');
+  const hasErrorMessage = !!errorMessage;
+  return {hasErrorMessage, errorMessage};
+}
+
+function checkUserInput() {
+  const hasErrorMessage = checkErrorMessage().hasErrorMessage;
+  
+  if (
+    !validator.isEmail(inputElement.value) &&
+    !hasErrorMessage &&
+    inputElement.value !== ""
+  ) {
+    const errorMessage = document.createElement("span");
+    errorMessage.classList.add('error-message')
+    errorMessage.innerText = "Valid email required";
+    containerMessage.appendChild(errorMessage);
+    inputElement.classList.add('error-message')
+  }
+  
+  else if(validator.isEmail(inputElement.value)) {
+    inputElement.classList.add('valid-email')
+  }
+}
+
+function changeState() {
+  const errorMessageObject = checkErrorMessage();
+  
+  if (errorMessageObject.hasErrorMessage) {
+    containerMessage.removeChild(errorMessageObject.errorMessage);
+    inputElement.classList.remove('error-message');
+  } 
+  else if(inputElement.classList.contains('valid-email')){
+    inputElement.classList.remove('valid-email');
+  }
+}
+
+window.addEventListener("load", checkScrollBar);
+
+window.addEventListener("resize", checkScrollBar);
+
+inputElement.addEventListener("blur", checkUserInput);
+
+inputElement.addEventListener("input", changeState);
